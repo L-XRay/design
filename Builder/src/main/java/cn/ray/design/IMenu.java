a5df9b258ac52938d4e95f58c2e932babab92476
Ray <2020215059@stu.cqupt.edu.cn>
2022-07-29 23:06:15 +0800

    complete Builder

---
+++ Builder/src/main/java/cn/ray/design/IMenu.java
@@ -0,0 +1,33 @@
+package cn.ray.design;
+
+/**
+ * @author JOJO
+ * @date 2022/7/29 22:52
+ */
+public interface IMenu {
+    /**
+     * 吊顶
+     */
+    IMenu appendCeiling(Matter matter);
+
+    /**
+     * 涂料
+     */
+    IMenu appendCoat(Matter matter);
+
+    /**
+     * 地板
+     */
+    IMenu appendFloor(Matter matter);
+
+    /**
+     * 地砖
+     */
+    IMenu appendTile(Matter matter);
+
+    /**
+     * 明细
+     */
+    String getDetail();
+
+}
